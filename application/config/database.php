ed5f022991e51130a38189268d036c685e839c2a
madsegaf <madsegaf44@gmail.com>
2015-06-23 09:34:31 +0700

    3

--- application/config/database.php
+++ application/config/database.php
@@ -48,10 +48,10 @@
 $active_group = 'default';
 $active_record = TRUE;
 
-$db['default']['hostname'] = 'mysql.idhostinger.com';
-$db['default']['username'] = 'u130873045_segaf';
-$db['default']['password'] = 'muhammadsegaf44';
-$db['default']['database'] = 'u130873045_mprs';
+$db['default']['hostname'] = 'localhost';
+$db['default']['username'] = '';
+$db['default']['password'] = '';
+$db['default']['database'] = '';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
--- application/config/routes.php
+++ application/config/routes.php
@@ -38,7 +38,7 @@
 |
 */
 
-$route['default_controller'] = "mprs";
+$route['default_controller'] = "welcome";
 $route['404_override'] = '';
 
 
