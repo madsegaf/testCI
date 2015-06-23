d0cd366f97e76311a97fca47ce6bea17565b8dc6
madsegaf <madsegaf44@gmail.com>
2015-06-23 09:25:25 +0700

    update1

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://mapresdb.16mb.com/';
+$config['base_url']	= 'http://testgitmapres.azurewebsites.net/';
 
 /*
 |--------------------------------------------------------------------------
